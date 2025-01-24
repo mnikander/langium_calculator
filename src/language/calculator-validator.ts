67b34facb7fb1905844f5bcead902b1fa9f5911a
Marco Nikander <m4n1k.developer@gmail.com>
2025-01-24 10:36:52 +0100

    scaffold a langium project

---
+++ src/language/calculator-validator.ts
@@ -0,0 +1,31 @@
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { CalculatorAstType, Person } from './generated/ast.js';
+import type { CalculatorServices } from './calculator-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: CalculatorServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.CalculatorValidator;
+    const checks: ValidationChecks<CalculatorAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class CalculatorValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
