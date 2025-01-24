f097248dbc4d5a1f9e91321be5628e9f490d7c24
Marco Nikander <m4n1k.developer@gmail.com>
2025-01-24 11:55:50 +0100

    disable validator for now

--- src/language/calculator-validator.ts
+++ src/language/calculator-validator.ts
@@ -1,5 +1,7 @@
-import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { CalculatorAstType, Person } from './generated/ast.js';
+import type { ValidationChecks } from 'langium';
+// import type { ValidationAcceptor, ValidationChecks } from 'langium';
+// import type { CalculatorAstType, Person } from './generated/ast.js';
+import type { CalculatorAstType } from './generated/ast.js';
 import type { CalculatorServices } from './calculator-module.js';
 
 /**
@@ -9,7 +11,7 @@ export function registerValidationChecks(services: CalculatorServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.CalculatorValidator;
     const checks: ValidationChecks<CalculatorAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        // Person: validator.checkPersonStartsWithCapital
     };
     registry.register(checks, validator);
 }
@@ -19,13 +21,13 @@ export function registerValidationChecks(services: CalculatorServices) {
  */
 export class CalculatorValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
+    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+    //     if (person.name) {
+    //         const firstChar = person.name.substring(0, 1);
+    //         if (firstChar.toUpperCase() !== firstChar) {
+    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+    //         }
+    //     }
+    // }
 
 }
