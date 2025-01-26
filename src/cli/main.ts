f42adcbfaaa52583335c0c94b443d807cb668dc2
Marco Nikander <m4n1k.developer@gmail.com>
2025-01-26 13:04:35 +0100

    comment out stubs generated by langium, for future features

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -3,8 +3,8 @@ import chalk from 'chalk';
 import { Command } from 'commander';
 import { CalculatorLanguageMetaData } from '../language/generated/module.js';
 import { createCalculatorServices } from '../language/calculator-module.js';
-import { extractAstNode, extractDocument } from './cli-util.js';
-import { generateCpp, generateJavaScript, generateJSON } from './generator.js';
+import { extractDocument } from './cli-util.js';
+import { generateCpp, generateJSON } from './generator.js';
 import { NodeFileSystem } from 'langium/node';
 import * as url from 'node:url';
 import * as fs from 'node:fs/promises';
@@ -14,12 +14,12 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const packagePath = path.resolve(__dirname, '..', '..', 'package.json');
 const packageContent = await fs.readFile(packagePath, 'utf-8');
 
-export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
-    const services = createCalculatorServices(NodeFileSystem).Calculator;
-    const model = await extractAstNode<Model>(fileName, services);
-    const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
-    console.log(chalk.green(`JavaScript code generated successfully: ${generatedFilePath}`));
-};
+// export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
+//     const services = createCalculatorServices(NodeFileSystem).Calculator;
+//     const model = await extractAstNode<Model>(fileName, services);
+//     const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
+//     console.log(chalk.green(`JavaScript code generated successfully: ${generatedFilePath}`));
+// };
 
 export type GenerateOptions = {
     destination?: string;
@@ -64,12 +64,12 @@ export default function(): void {
     program.version(JSON.parse(packageContent).version);
 
     const fileExtensions = CalculatorLanguageMetaData.fileExtensions.join(', ');
-    program
-        .command('generate')
-        .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
-        .option('-d, --destination <dir>', 'destination directory of generating')
-        .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
-        .action(generateAction);
+    // program
+    //     .command('generate')
+    //     .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
+    //     .option('-d, --destination <dir>', 'destination directory of generating')
+    //     .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
+    //     .action(generateAction);
 
     program
         .command('toJSON')
